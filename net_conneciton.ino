24b7789ef0e64eadd6f6acf87e9caa42af8d68f7
Robert Queenin <queenin@gmail.com>
2016-07-31 01:06:19 -0400

    copypasta IMU example

--- net_conneciton.ino
+++ net_conneciton.ino
@@ -1,42 +1,51 @@
-/*
- *  Simple HTTP get webclient test
- */
-
-#include <ESP8266WiFi.h>
-#include <credentials.h>
 #include <Wire.h>
-#include <Adafruit_INA219.h>
 
-Adafruit_INA219 ina219;
+#include <Adafruit_Sensor.h>
+#include <Adafruit_LSM303_U.h>
+#include <Adafruit_L3GD20_U.h>
+#include <Adafruit_9DOF.h>
+#include <ESP8266WiFi.h>
 
-#if defined(ARDUINO_ARCH_SAMD)
-    // for Zero, output on USB Serial console, remove line below 
-    // if using programming port to program the Zero!
-    #define Serial SerialUSB
-#endif
+#include <credentials.h>
 
 const char* ssid     = credentials::ssid;
 const char* password = credentials:password;
 
 const char* host = "www.adafruit.com";
 
-void setup() {
+/* Assign a unique ID to the sensors */
+Adafruit_9DOF                dof   = Adafruit_9DOF();
+Adafruit_LSM303_Accel_Unified accel = Adafruit_LSM303_Accel_Unified(30301);
+Adafruit_LSM303_Mag_Unified   mag   = Adafruit_LSM303_Mag_Unified(30302);
 
+/* Update this with the correct SLP for accurate altitude measurements */
+float seaLevelPressure = SENSORS_PRESSURE_SEALEVELHPA;
+
+
+void initSensors()
+{
+  if(!accel.begin())
+  {
+    /* There was a problem detecting the LSM303 ... check your connections */
+    Serial.println(F("Ooops, no LSM303 detected ... Check your wiring!"));
+    while(1);
+  }
+  if(!mag.begin())
+  {
+    /* There was a problem detecting the LSM303 ... check your connections */
+    Serial.println("Ooops, no LSM303 detected ... Check your wiring!");
+    while(1);
+  }
+}
+
+
+void setup() {
   Serial.begin(115200);
   delay(100);
-  
-  #ifndef ESP8266
-    while (!Serial);     // will pause Zero, Leonardo, etc until serial console opens
-  #endif
-  uint32_t currentFrequency;
-    
-  Serial.begin(115200);
-  Serial.println("Hello!");
-  
-  ina219.begin();
-  Serial.println("Measuring voltage and current with INA219 ..."); 
+
 
   // We start by connecting to a WiFi network
+
   Serial.println();
   Serial.println();
   Serial.print("Connecting to ");
@@ -53,29 +62,14 @@ void setup() {
   Serial.println("WiFi connected");  
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
+
+  /* Initialise the IMU sensors */
+  initSensors();
 }
 
-int value = 0;
 
 void loop() {
   delay(5000);
-  ++value;
-
-  float shuntvoltage = 0;
-  float busvoltage = 0;
-  float current_mA = 0;
-  float loadvoltage = 0;
-
-  shuntvoltage = ina219.getShuntVoltage_mV();
-  busvoltage = ina219.getBusVoltage_V();
-  current_mA = ina219.getCurrent_mA();
-  loadvoltage = busvoltage + (shuntvoltage / 1000);
-
-  Serial.print("Bus Voltage:   "); Serial.print(busvoltage); Serial.println(" V");
-  Serial.print("Shunt Voltage: "); Serial.print(shuntvoltage); Serial.println(" mV");
-  Serial.print("Load Voltage:  "); Serial.print(loadvoltage); Serial.println(" V");
-  Serial.print("Current:       "); Serial.print(current_mA); Serial.println(" mA");
-  Serial.println("");
 
   Serial.print("connecting to ");
   Serial.println(host);
@@ -108,5 +102,36 @@ void loop() {
   Serial.println();
   Serial.println("closing connection");
 
-  delay(2000);
+
+  // START IMU LOOP
+  sensors_event_t accel_event;
+  sensors_event_t mag_event;
+  sensors_vec_t   orientation;
+
+  /* Calculate pitch and roll from the raw accelerometer data */
+  accel.getEvent(&accel_event);
+  if (dof.accelGetOrientation(&accel_event, &orientation))
+  {
+    /* 'orientation' should have valid .roll and .pitch fields */
+    Serial.print(F("Roll: "));
+    Serial.print(orientation.roll);
+    Serial.print(F("; "));
+    Serial.print(F("Pitch: "));
+    Serial.print(orientation.pitch);
+    Serial.print(F("; "));
+  }
+  
+  /* Calculate the heading using the magnetometer */
+  mag.getEvent(&mag_event);
+  if (dof.magGetOrientation(SENSOR_AXIS_Z, &mag_event, &orientation))
+  {
+    /* 'orientation' should have valid .heading data now */
+    Serial.print(F("Heading: "));
+    Serial.print(orientation.heading);
+    Serial.print(F("; "));
+  }
+
+  Serial.println(F(""));
+  delay(100);
+  // END IMU LOOP
 }
