82caf77c60014402b3c1edbc50b0f9f0b67cf01e
Robert Queenin <queenin@gmail.com>
2016-08-01 02:47:20 -0400

    cleaning up the hello world

--- read_upload/read_upload.ino
+++ read_upload/read_upload.ino
@@ -9,25 +9,20 @@
 
 #include <ESP8266WiFi.h>
 
-#include "credentials.h"
+#include "config.h"
 
 Adafruit_INA219 ina219;
 
-//const char* ssid     = credentials::ssid;
-//const char* password = credentials:password;
-
-const char* host = "www.adafruit.com";
-
 /* Assign a unique ID to the sensors */
-Adafruit_9DOF                dof   = Adafruit_9DOF();
+Adafruit_9DOF dof = Adafruit_9DOF();
 Adafruit_LSM303_Accel_Unified accel = Adafruit_LSM303_Accel_Unified(30301);
-Adafruit_LSM303_Mag_Unified   mag   = Adafruit_LSM303_Mag_Unified(30302);
+Adafruit_LSM303_Mag_Unified mag = Adafruit_LSM303_Mag_Unified(30302);
 
 /* Update this with the correct SLP for accurate altitude measurements */
 float seaLevelPressure = SENSORS_PRESSURE_SEALEVELHPA;
 
 
-void initSensors()
+void initIMU()
 {
   if(!accel.begin())
   {
@@ -43,45 +38,7 @@ void initSensors()
   }
 }
 
-
-void setup() {
-  Serial.begin(115200);
-  delay(100);
-  
-  uint32_t currentFrequency;
-    
-  Serial.begin(115200);
-  Serial.println("Hello!");
-  
-  ina219.begin();
-  Serial.println("Measuring voltage and current with INA219 ..."); 
-
-  // We start by connecting to a WiFi network
-  Serial.println();
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-  
-  WiFi.begin(ssid, password);
-  
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-
-  Serial.println("");
-  Serial.println("WiFi connected");  
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
-
-  /* Initialise the IMU sensors */
-  initSensors();
-}
-
-
-void loop() {
-  //delay(5000);
-
+void readINA219() {
   float shuntvoltage = 0;
   float busvoltage = 0;
   float current_mA = 0;
@@ -97,41 +54,10 @@ void loop() {
   Serial.print("Load Voltage:  "); Serial.print(loadvoltage); Serial.println(" V");
   Serial.print("Current:       "); Serial.print(current_mA); Serial.println(" mA");
   Serial.println("");
+}
 
-  Serial.print("connecting to ");
-  Serial.println(host);
-  
-  // Use WiFiClient class to create TCP connections
-  WiFiClient client;
-  const int httpPort = 80;
-  if (!client.connect(host, httpPort)) {
-    Serial.println("connection failed");
-    return;
-  }
-  
-  // We now create a URI for the request
-  String url = "/testwifi/index.html";
-  Serial.print("Requesting URL: ");
-  Serial.println(url);
-  
-  // This will send the request to the server
-  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
-               "Host: " + host + "\r\n" + 
-               "Connection: close\r\n\r\n");
-  delay(500);
-  
-  // Read all the lines of the reply from server and print them to Serial
-  while(client.available()){
-    String line = client.readStringUntil('\r');
-    Serial.print(line);
-  }
-  
-  Serial.println();
-  Serial.println("closing connection");
+void readIMU() {
 
-  //delay(2000);
-
-  // START IMU LOOP
   sensors_event_t accel_event;
   sensors_event_t mag_event;
   sensors_vec_t   orientation;
@@ -160,6 +86,59 @@ void loop() {
   }
 
   Serial.println(F(""));
+}
+
+
+void setup() {
+  Serial.begin(115200);
   delay(100);
-  // END IMU LOOP
+  
+  uint32_t currentFrequency;
+  
+  ina219.begin();
+
+  initIMU();
+
+  // wifi connection
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("wifi connected");  
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+
+void loop() {
+
+  readINA219();
+
+  readIMU();
+
+  Serial.print("connecting to ");
+  Serial.println(host);
+  
+  // Use WiFiClient class to create TCP connections
+  WiFiClient client;
+  const int httpPort = 80;
+  if (!client.connect(host, httpPort)) {
+    Serial.println("connection failed");
+    return;
+  }
+ 
+  // request
+  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+               "Host: " + host + "\r\n" + 
+               "Connection: close\r\n\r\n");
+  // yield?
+  delay(500);
+  
+  while(client.available()){
+    String line = client.readStringUntil('\r');
+    Serial.print(line);
+  }
 }
